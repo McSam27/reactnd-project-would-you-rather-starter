3f30a14a68465f3bf4f3c0e75d5b53027a9879ff
Sam McCagg <sam.mccagg@ellucian.com>
2018-12-02 21:17:25 -0500

    cleanup questions reducers.,

--- src/reducers/questionReducers.js
+++ src/reducers/questionReducers.js
@@ -11,22 +11,33 @@ function questions(state = {}, action) {
         ...state,
         ...action.questions
       }
-      case ADD_QUESTION:
-        return {
-          ...state,
-          [action.question.id]: action.question
-        }
-      case SAVE_QUESTION_ANSWER:
-      const { qid, answer, authedUser } = action;
-        return {
-          ...state,
-          [qid]: {
-            ...state[qid],
-            [answer]: {
-              ...state[qid][answer],
-              votes: [...state[qid][answer].votes, authedUser],
+    case ADD_QUESTION:
+      return {
+        ...state,
+        [action.question.id]: action.question
+      }
+    case SAVE_QUESTION_ANSWER:
+      const {
+        qid,
+        answer,
+        authedUser
+      } = action;
+      // if authedUser votes on an unanswered poll
+      if (state[qid].optionOne.votes.indexOf(authedUser) === -1 &&
+        state[qid].optionTwo.votes.indexOf(authedUser) === -1) {
+          return {
+            ...state,
+            [qid]: {
+              ...state[qid],
+              [answer]: {
+                ...state[qid][answer],
+                votes: [...state[qid][answer].votes, authedUser],
+              },
             },
-          },        }
+          };
+      } else {
+        return { ...state };
+      }
     default:
       return state
   }
